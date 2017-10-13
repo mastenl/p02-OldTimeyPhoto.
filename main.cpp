b69f85bee9cabcbf67271f747831bbd01076e37b
mastenl <mleonard14@comcast.net>
2017-10-13 13:47:02 -0700

    makes sure it is an image you can open.

--- main.cpp
+++ main.cpp
@@ -3,36 +3,61 @@
 #include <string>
 #include "bitmap.h"
 #include <cmath>
+#include <ctime>
 using namespace std;
 
+ //machupicchu.bmp
 int main()
 {
 Bitmap image;
 vector <vector <Pixel> > bmp;
 Pixel rgb;
 string fileName;
+char grain;
+bool validBmp;
+srand ( time(NULL) );
+int greyOffset=25;
 
-      //read a file example.bmp and convert it to a pixel matrix
-cout << "Please enter the file name. \n";
-cin >> fileName;
+//  cout <<'\v'<<"This file can not be read."<<endl;
+//  cout << "The file must be an accesible 24 bit depth Windows BMP file." << endl;
 
-image.open(fileName);
-bmp = image.toPixelMatrix();
+//read a file example.bmp and convert it to a pixel matrix
+do
+{
+  cout << "Please enter the file name. \n";
+  cin >> fileName;
+  image.open(fileName);
+  validBmp = image.isImage();
+  if (validBmp == false)//tells you if you fucked up
+  {
+    cout <<'\v'<<"This file can not be read."<<endl;
+    cout << "The file must be an accesible 24 bit depth Windows BMP file." << endl;
+  }
+}
+while(validBmp == false );
 
-cout << fileName << " has been loaded." << " It is " << bmp.size() <<" pixels high ";
-cout << "and " << bmp[0].size() << " pixels wide. "<< endl;
+
+if( validBmp == true )
+ {
+   bmp = image.toPixelMatrix();
+
+
+//random num film grain works
+cout <<"Would you like to add film grain? (Y)es, (N)o " <<endl;
+cin >> grain;
+int randomNum;
 
  int r;
  int g;
  int b;
  int grey;
 
-
   for(int i = 0; i < bmp.size(); i++)
   {
 
     for(int j = 0; j < bmp[i].size(); j++)
     {
+
       r = bmp[i][j].red;
       g = bmp[i][j].green;
       b = bmp[i][j].blue;
@@ -41,21 +66,41 @@ cout << "and " << bmp[0].size() << " pixels wide. "<< endl;
 
       grey = (r+g+b)/3;
 
+      if (grain=='y' || grain=='Y')//adds film grain
+      {
+        randomNum = rand() % 60;
+        grey = grey-randomNum;
+        grey=grey+greyOffset;// greyOffset
+      }
+      if(grey<0)
+      {
+        grey=0;
+      }
+      if(grey>255)
+      {
+        grey=255;
+      }
+
       rgb.red = grey;
       rgb.green = grey;
       rgb.blue = grey;
 
-       //machupicchu.bmp
+
       bmp[i][j] = rgb;
+
 
     }
   }
   //update the bitmap and save it
 
+  cout << "It is " << bmp.size() <<" pixels high ";
+  cout << "and " << bmp[0].size() << " pixels wide. "<< endl;
+
 image.fromPixelMatrix(bmp);
 
-image.save("oldTimey.bmp");
+image.save("filmGrain.bmp");
 cout << "end";
+}
 
 return 0;
 }
